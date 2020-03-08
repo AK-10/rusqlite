d21480f524d9538dc975ceec05e238bec75bfbb4
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-03-08 15:19:49 +0900

    trim

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
-use std::io;
-use std::process;
+use std::{ io, process };
+use std::io::Write;
 
 fn main() {
-    println!("Hello, world!");
-
-    let mut query = String::new();
     loop {
+        let mut query = String::new();
+
+        print!("rusqlite> ");
+        // printはデフォルトでラインバッファに溜まるので明示的にflushする
+        io::stdout().flush().unwrap();
+
         io::stdin().read_line(&mut query).expect("failed to read line");
-        if query == ".exit" {
+        if query.trim_end() == ".exit" {
             println!("bye✋");
-            std::process::exit(0);
+            process::exit(0);
         } else {
-            println!("Unrecognized command {}.", query);
+            println!("Unrecognized command: {}", query);
         }
     }
 }
