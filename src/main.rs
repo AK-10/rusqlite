832209d8995afee0c46d78ecaa5d54cef01bc0f5
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-03-08 14:56:47 +0900

    impl weak repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+use std::process;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut query = String::new();
+    loop {
+        io::stdin().read_line(&mut query).expect("failed to read line");
+        if query == ".exit" {
+            println!("bye✋");
+            std::process::exit(0);
+        } else {
+            println!("Unrecognized command {}.", query);
+        }
+    }
 }
