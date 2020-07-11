6e5f4a80cf710e038386d94e5e5c36311848efc7
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-07-11 12:40:14 +0900

    finish part12

--- c/db.c
+++ c/db.c
@@ -155,7 +155,11 @@ const uint8_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_P
 // cellはkey/valueのペアを指す.
 const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
 const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
-const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
+const uint32_t LEAF_NODE_NEXT_LEAF_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NEXT_LEAF_OFFSET =
+        LEAF_NODE_NUM_CELLS_OFFSET + LEAF_NODE_NUM_CELLS_SIZE;
+const uint32_t LEAF_NODE_HEADER_SIZE =
+        COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE + LEAF_NODE_NEXT_LEAF_SIZE;
 
 /*
  * Leaf Node Body Layout
@@ -262,6 +266,10 @@ void print_tree(Pager*, uint32_t, uint32_t);
 // ========= part11 start ===========
 Cursor* internal_node_find(Table*, uint32_t, uint32_t);
 // ========= part11 end ===========
+
+// ========= part12 start ===========
+uint32_t* leaf_node_next_leaf(void*);
+// ========= part12 end ===========
 
 const uint32_t LEAF_NODE_RIGHT_SPLIT_COUNT = (LEAF_NODE_MAX_CELLS + 1) / 2;
 const uint32_t LEAF_NODE_LEFT_SPLIT_COUNT = (LEAF_NODE_MAX_CELLS + 1) - LEAF_NODE_RIGHT_SPLIT_COUNT;
@@ -632,22 +640,33 @@ void pager_flush(Pager* pager, uint32_t page_num) {
     }
 }
 
-Cursor* table_start(Table* table) {
-    Cursor* cursor = malloc(sizeof(Cursor));
-    cursor->table = table;
-    cursor->page_num = table->root_page_num;
-    cursor->cell_num = 0;
+// 常にルートノードを見るようになっている
+// ルートノードが中間ノードだと行のデータを持っていないので，おかしくなる
+// Cursor* table_start(Table* table) {
+//     Cursor* cursor = malloc(sizeof(Cursor));
+//     cursor->table = table;
+//     cursor->page_num = table->root_page_num;
+//     cursor->cell_num = 0;
 
-    void* root_node = get_page(table->pager, table->root_page_num);
-    uint32_t num_cells = *leaf_node_num_cells(root_node);
+//     void* root_node = get_page(table->pager, table->root_page_num);
+//     uint32_t num_cells = *leaf_node_num_cells(root_node);
+//     cursor->end_of_table = (num_cells == 0);
+
+//     return cursor;
+// }
+
+Cursor* table_start(Table* table) {
+    Cursor* cursor = table_find(table, 0);
+
+    void* node = get_page(table->pager, cursor->page_num);
+    uint32_t num_cells = *leaf_node_num_cells(node);
     cursor->end_of_table = (num_cells == 0);
 
     return cursor;
 }
 
-
 /*
- * Return the position of the given key.
+ * Return the position(ノードへのカーソル) of the given key.
  * If the key is not present, return the position
  * where it should be inserted
  */
@@ -668,7 +687,15 @@ void cursor_advance(Cursor* cursor) {
 
     cursor->cell_num += 1;
     if (cursor->cell_num >= (*leaf_node_num_cells(node))) {
-        cursor->end_of_table = true;
+        // 次のノードへ進める
+        uint32_t next_page_num = *leaf_node_next_leaf(node);
+        if (next_page_num == 0) {
+            // 木全体におけるもっとも右の葉ノード
+            cursor->end_of_table = true;
+        } else {
+            cursor->page_num = next_page_num;
+            cursor->cell_num = 0;
+        }
     }
 }
 
@@ -693,6 +720,7 @@ void initialize_leaf_node(void* node) {
     set_node_type(node, NODE_LEAF);
     set_node_root(node, false);
     *leaf_node_num_cells(node) = 0;
+    *leaf_node_next_leaf(node) = 0; // 0 represents no sibling
 }
 
 void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
@@ -787,6 +815,8 @@ void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value) {
     uint32_t new_page_num = get_unused_page_num(cursor->table->pager);
     void* new_node = get_page(cursor->table->pager, new_page_num);
     initialize_leaf_node(new_node);
+    *leaf_node_next_leaf(new_node) = *leaf_node_next_leaf(old_node);
+    *leaf_node_next_leaf(old_node) = new_page_num;
 
     /*
     All existing keys plus new key should be divided
@@ -806,7 +836,8 @@ void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value) {
         void* destination = leaf_node_cell(destination_node, index_within_node);
 
         if (i == cursor->cell_num) {
-            serialize_row(value, destination);
+            serialize_row(value, leaf_node_value(destination_node, index_within_node));
+            *leaf_node_key(destination_node, index_within_node) = key;
         } else if (i > cursor->cell_num) {
             memcpy(destination, leaf_node_cell(old_node, i-1), LEAF_NODE_CELL_SIZE);
         } else {
@@ -977,6 +1008,10 @@ Cursor* internal_node_find(Table* table, uint32_t page_num, uint32_t key) {
         case NODE_INTERNAL:
             return internal_node_find(table, child_num, key);
     }
+}
+
+uint32_t* leaf_node_next_leaf(void* node) {
+    return node + LEAF_NODE_NEXT_LEAF_OFFSET;
 }
 
 int main(int argc, char* argv[]) {
